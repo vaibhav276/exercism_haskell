e2a46b0bec7ad1d3fde62f7da9a6eff8b723bc1c
Vaibhav Pujari <vaibhav276@yahoo.co.in>
2017-01-09 16:14:37 +0530

    Nucleotide Count

---
+++ nucleotide-count/src/DNA.hs
@@ -0,0 +1,28 @@
+module DNA (count, nucleotideCounts) where
+
+import Data.Map (Map, lookup, fromList, update, member)
+import Control.Monad (foldM)
+
+initMap :: Map Char Int
+initMap = fromList [('A',0),('G',0),('T',0),('C',0)]
+
+strInvalidNucleotide :: String
+strInvalidNucleotide = "Invalid Nucleotide"
+
+count :: Char -> String -> Either String Int
+count c xs = do
+    m <- nucleotideCounts xs 
+    maybeToEither strInvalidNucleotide (Data.Map.lookup c m)
+
+nucleotideCounts :: String -> Either String (Map Char Int)
+nucleotideCounts = foldM mapStep initMap
+
+mapStep :: (Map Char Int) -> Char -> Either String (Map Char Int)
+mapStep m c = case member c initMap of
+    True  -> Right $ update (\v -> Just (v + 1)) c m
+    False -> Left strInvalidNucleotide
+
+maybeToEither :: a -> Maybe b -> Either a b
+maybeToEither s x = case x of
+    Just y  -> Right y
+    Nothing -> Left s
